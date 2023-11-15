7e87119249aa2be40959596cf6ab28c2cc31b5bb
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-11-15 16:19:40 +0700

    tested on Samsung M23

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,61 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from "vue";
+declare global {
+  interface Window {
+    NDEFReader: any;
+  }
+}
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const logBox = ref("");
+
+function log(message: string) {
+  logBox.value += message + "\n";
+}
+
+async function scan() {
+  log("User clicked scan button");
+
+  try {
+    const ndef = new window.NDEFReader();
+    await ndef.scan();
+    log("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }: any) => {
+      log(`> Serial Number: ${serialNumber}`);
+      log(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    log("Argh! " + error);
+  }
+}
+
+const computedLogs = computed(() => {
+  return !("NDEFReader" in window)
+    ? "Web NFC is not available. Use Chrome on Android."
+    : logBox.value;
+});
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="scan">Scan</button>
+    <!-- <button type="button" @click="write">Write</button>
+    <button type="button" @click="makeReadOnly">Make Read-Only</button> -->
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>
+    <textarea name="logs" id="logs" cols="30" rows="10">
+      {{ computedLogs }}
+    </textarea>
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+declare global {
+  interface Window {
+    NDEFReader: any;
+  }
+}
+
 createApp(App).mount('#app')
